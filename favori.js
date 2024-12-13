42e69985cc01c3a7eb784d16b38d614dcc48cde4
GuzidYagmur <guzideyagmur@hotmail.com>
2024-12-13 19:53:29 +0300

    favori filmler arrayi

---
+++ favori.js
@@ -0,0 +1,10 @@
+let favoriFilmler=[];
+
+
+let sayi= prompt("Kaç tane favori filminiz var");
+for(i=0;i<sayi; i++){
+    let filmler = prompt("Favori filmlerinizden birini giriniz");
+    favoriFilmler.push(filmler);
+}
+console.log(favoriFilmler);
+
